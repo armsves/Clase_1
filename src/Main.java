655804ca2d5e8a67b9098fc18a7935171aa6b6f9
Armando Medina <armsves@gmail.com>
2022-09-19 18:00:19 +0200

    Test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    private static String productFormatter(String product_name, String date) {
+        String string = product_name.replace(" ", "_") + "_" + date;
+        return string.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(productFormatter("tenemos un producto aqui", "16/09/1982"));
+    }
+
+}
